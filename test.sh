ab16d7fca535fd77c30c676aa128c95e48cb6c16
wn789 <768187171@qq.com>
2017-11-25 20:54:02 +0800

    Add files via upload

--- test.sh
+++ test.sh
@@ -32,19 +32,20 @@ next() {
 }
 
 
-echo "欢迎使用一键VPS性能检测，继续请按回车！[default:Enter]"
+echo "服务器提供商（host provider）[default:Enter]"
 read hostp
-echo "开始测试，如果服务器带宽比较低可能会需要较长点时间，请耐心等待！"
+echo "开始测试中，会需要点时间，请稍后"
 
 
 _included_benchmarks=""
-
+upfile="y"
 
 
 #取参数
-while getopts "i:" opt; do
+while getopts "i:u" opt; do
     case $opt in
         i) _included_benchmarks=$OPTARG;;
+		u) upfile="n";;
     esac
 done
 
@@ -60,26 +61,26 @@ bt="benchtest"
 if [[ $_included_benchmarks == *$bt* ]]
 then
     apt-get >/dev/null 2>&1
-	[ $? -le '1' ] && ( apt-get update | apt-get -y install curl mtr virt-what python perl automake autoconf time make gcc gdb )
+	[ $? -le '1' ] && ( apt-get update | apt-get -y install curl mtr-tiny virt-what python perl automake autoconf time make gcc gdb )
 	yum >/dev/null 2>&1
 	[ $? -le '1' ] && yum -y install which sed curl mtr virt-what python make gcc gcc-c++ gdbautomake autoconf time perl-Time-HiRes perl
 else
     apt-get >/dev/null 2>&1
-	[ $? -le '1' ] && ( apt-get update | apt-get -y install curl mtr virt-what python )
+	[ $? -le '1' ] && ( apt-get update | apt-get -y install curl mtr-tiny virt-what python )
 	yum >/dev/null 2>&1
 	[ $? -le '1' ] && yum -y install which sed curl mtr virt-what python
 fi
 
 #要用到的变量
 backtime=`date +%Y%m%d`
-logfilename="testwn789.log"
+logfilename="91yuntest.log"
 dir=`pwd`
 IP=$(curl -s myip.ipip.net | awk -F ' ' '{print $2}' | awk -F '：' '{print $2}')
 echo "====开始记录测试信息====">${dir}/$logfilename
 
 #创建测试目录
-mkdir -p wn789test
-cd wn789test
+mkdir -p 91yuntest
+cd 91yuntest
 
 clear
 
@@ -89,10 +90,25 @@ arr=(${_included_benchmarks//,/ })
 #下载执行相应的代码
 for i in ${arr[@]}    
 do 
-	wget -q --no-check-certificate https://raw.githubusercontent.com/wn789/vpsxn/test/test_code/${i}.sh
-    . ${dir}/vpsxn/${i}.sh
+	wget -q --no-check-certificate https://raw.githubusercontent.com/91yun/91yuntest/test/test_code/${i}.sh
+    . ${dir}/91yuntest/${i}.sh
 	eval ${i}
 done    
 
+
+#上传文件
+updatefile()
+{
+	resultstr=$(curl -s -T ${dir}/$logfilename "http://logfileupload.91yuntest.com/logfileupload.php")
+	echo -e $resultstr | tee -a ${dir}/$logfilename
+}
+
+if [[ $upfile == "y" ]]
+then
+	updatefile
+else
+	echo "测试结束，具体日志查看 91yuntest.log"
+fi
 #删除目录
-rm -rf ${dir}/wn789test
+rm -rf ${dir}/91yuntest
+
